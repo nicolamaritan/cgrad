f77f3f1e120a7ccc79045a9f487207e25aba2b59
Nicola Maritan <nicolamaritan01@gmail.com>
2025-04-14 02:29:06 +0200

    Polished example.
    Missing relu

--- examples/mlp_example.c
+++ examples/mlp_example.c
@@ -13,20 +13,19 @@
 #include <math.h>
 
 // Example 2-layer-friendly function: y = tanh(w·x + b)
-double compute_y(double *x_row, double *weights, double bias, size_t dim) 
-{
-    double dot = 0.0;
-    for (size_t j = 0; j < dim; j++) {
-        dot += x_row[j] * weights[j];
-    }
-    return tanh(dot + bias);
-}
+double compute_y(double *x_row, double *weights, double bias, size_t dim);
+
+// Example dataset build
+void build_dataset(tensor *x, tensor *y_target);
+
 int main()
 {
     init_random();
 
     const size_t batch_size = 128;
     const size_t input_dim = 4;
+    const size_t hidden_dim = 8;
+    const size_t out_dim = 1;
 
     tensor *x = tensor2d_alloc(batch_size, input_dim);
     tensor *y_target = tensor2d_alloc(batch_size, 1);
@@ -35,41 +34,17 @@ int main()
         tensor_free(y_target);
         return 1; 
     }
-    
-    // Random weights and bias for generating y
-    double lb = -5;
-    double ub = 5;
-    double weights[4] = {sample_uniform(lb, ub), sample_uniform(lb, ub), sample_uniform(lb, ub), sample_uniform(lb, ub)};
-    double bias = sample_uniform(lb, ub);
-    
-    // Populate x with random values and compute y
-    for (size_t i = 0; i < batch_size; i++) {
-        double x_row[4];
-        for (size_t j = 0; j < 4; j++) {
-            double value = sample_uniform(lb, ub);
-            x_row[j] = value;
-            tensor2d_set_unchecked(x, i, j, value);
-        }
-    
-        double y_value = compute_y(x_row, weights, bias, input_dim);
-        tensor2d_set_unchecked(y_target, i, 0, y_value);
-    }
 
-    printf("x:\n");
-    print_tensor(x);
-    printf("y_target:\n");
-    print_tensor(y_target);
+    build_dataset(x, y_target);
 
-    size_t in_dim_1 = 4;
-    size_t out_dim_1 = 8;
-    linear_layer *linear1 = linear_create(in_dim_1, out_dim_1);
+    // Allocate model
+    linear_layer *linear1 = linear_create(input_dim, hidden_dim);
     linear_xavier_init(linear1);
 
-    size_t in_dim_2 = 8;
-    size_t out_dim_2 = 1;
-    linear_layer *linear2 = linear_create(in_dim_2, out_dim_2);
+    linear_layer *linear2 = linear_create(hidden_dim, out_dim);
     linear_xavier_init(linear2);
 
+    // Setup model params
     model_params params;
     init_model_params(&params);
     add_model_param(&params, linear1->weights);
@@ -77,20 +52,25 @@ int main()
     add_model_param(&params, linear2->biases);
     add_model_param(&params, linear2->biases);
 
+    // Setup optimizer
     sgd_state opt_state;
     if (init_sgd_state(&opt_state, &params) != NO_ERROR)
         exit(1);
 
-    size_t epochs = 10000;
+    double lr = 3e-4;
+    double momentum = 0.9;
+
+    size_t epochs = 100;
     for (size_t i = 0; i < epochs; i++)
     {
-        tensor *mult1 = tensor2d_alloc(batch_size, out_dim_1);
-        tensor *h1 = tensor2d_alloc(batch_size, out_dim_1);
+        // ------------- Forward -------------
+        tensor *mult1 = tensor2d_alloc(batch_size, hidden_dim);
+        tensor *h1 = tensor2d_alloc(batch_size, hidden_dim);
         if (linear_forward_graph(x, linear1, mult1, h1) != NO_ERROR)
             exit(1);
 
-        tensor *mult2 = tensor2d_alloc(batch_size, out_dim_2);
-        tensor *h2 = tensor2d_alloc(batch_size, out_dim_2);
+        tensor *mult2 = tensor2d_alloc(batch_size, out_dim);
+        tensor *h2 = tensor2d_alloc(batch_size, out_dim);
         if (linear_forward_graph(h1, linear2, mult2, h2) != NO_ERROR)
             exit(1);
 
@@ -98,19 +78,19 @@ int main()
         if (mse_loss_graph(h2, y_target, z) != NO_ERROR)
             exit(1);
 
-        printf("loss: %f\n", z->data[0]);
+        printf("epoch %ld, loss: %f\n", i, z->data[0]);
 
+        // ------------- Backward -------------
         zero_grad(&params);        
         backward(z, false);
+        sgd_step(lr, momentum, false, &opt_state, &params);
 
-        sgd_step(0.0003, 0.9, false, &opt_state, &params);
-
+        // Clear iteration allocations
         tensor_free(h1);
         tensor_free(mult1);
         tensor_free(h2);
         tensor_free(mult2);
         tensor_free(z);
-
     }
 
     // Cleanup
@@ -118,4 +98,35 @@ int main()
     tensor_free(y_target);
     linear_free(linear1);
     return 0;
+}
+
+double compute_y(double *x_row, double *weights, double bias, size_t dim) 
+{
+    double dot = 0.0;
+    for (size_t j = 0; j < dim; j++) {
+        dot += x_row[j] * weights[j];
+    }
+    return tanh(dot + bias);
+}
+
+void build_dataset(tensor *x, tensor *y_target)
+{
+    // Random weights and bias for generating y
+    double lb = -5;
+    double ub = 5;
+    double weights[4] = {sample_uniform(lb, ub), sample_uniform(lb, ub), sample_uniform(lb, ub), sample_uniform(lb, ub)};
+    double bias = sample_uniform(lb, ub);
+    
+    // Populate x with random values and compute y
+    for (size_t i = 0; i < x->shape[0]; i++) {
+        double x_row[4];
+        for (size_t j = 0; j < 4; j++) {
+            double value = sample_uniform(lb, ub);
+            x_row[j] = value;
+            tensor2d_set_unchecked(x, i, j, value);
+        }
+    
+        double y_value = compute_y(x_row, weights, bias, x->shape[1]);
+        tensor2d_set_unchecked(y_target, i, 0, y_value);
+    }
 }
