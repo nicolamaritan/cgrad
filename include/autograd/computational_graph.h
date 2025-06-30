2410e665b0bbc85e5c1f04ce8e9c6ae6a577264f
Nicola Maritan <nicolamaritan01@gmail.com>
2025-06-30 14:15:08 +0200

    Removed typedef of computational_graph_node. Fixed error handling and not freed allocations

--- include/autograd/computational_graph.h
+++ include/autograd/computational_graph.h
@@ -6,7 +6,7 @@
 #include "config.h"
 #include <stdbool.h>
 
-typedef struct computational_graph_node computational_graph_node;
+struct computational_graph_node;
 
 /**
  * @struct computational_graph_node
@@ -16,26 +16,26 @@ typedef struct computational_graph_node computational_graph_node;
  * parents, children, and backpropagation functions. It is used to track the flow of data and
  * gradients during forward and backward passes in neural networks.
  */
-typedef struct computational_graph_node
+struct computational_graph_node
 {
     tensor *t;                                   /**< Pointer to the tensor associated with this node. */
     size_t n_parents;                            /**< Number of parent nodes. */
     size_t n_children;                           /**< Number of child nodes. */
-    computational_graph_node *parents[AUTOGRAD_MAX_PARENTS];  /**< Array of parent nodes. */
+    struct computational_graph_node *parents[AUTOGRAD_MAX_PARENTS];  /**< Array of parent nodes. */
     size_t parents_operands[AUTOGRAD_MAX_PARENTS];            /**< Operands associated with each parent. */
-    computational_graph_node *children[AUTOGRAD_MAX_CHILDREN];/**< Array of child nodes. */
+    struct computational_graph_node *children[AUTOGRAD_MAX_CHILDREN];/**< Array of child nodes. */
     tensor *tensor_operands[AUTOGRAD_MAX_CHILDREN];             /**< Tensors pointed by the computational graph nodes of the children. This is used to avoid recomputation of the operands in build_gradient() in backpropagation.h even if redundant with children attribute.*/
     backpropagation_function function[AUTOGRAD_MAX_CHILDREN]; /**< Backpropagation functions for each child. */
     bool is_involved_in_backprop;                /**< Flag indicating if the node is involved in backpropagation. */
     bool is_grad_computed;                       /**< Flag indicating if the gradient has been computed. */
-} computational_graph_node;
+};
 
 /**
  * @brief Allocates and initializes a new computational graph node.
  *
  * @return A pointer to the newly allocated computational graph node.
  */
-computational_graph_node *computational_graph_node_alloc();
+struct computational_graph_node *computational_graph_node_alloc();
 
 /**
  * @brief Allocates and initializes a new computational graph node with an associated tensor.
@@ -43,14 +43,14 @@ computational_graph_node *computational_graph_node_alloc();
  * @param t The tensor to associate with the new node.
  * @return A pointer to the newly allocated computational graph node.
  */
-computational_graph_node *computational_graph_node_tensor_alloc(tensor *const t);
+struct computational_graph_node *computational_graph_node_tensor_alloc(tensor *const t);
 
 /**
  * @brief Frees the memory allocated for a computational graph node.
  *
  * @param node The node to free.
  */
-void free_computational_graph_node(computational_graph_node *const node);
+void free_computational_graph_node(struct computational_graph_node *const node);
 
 /**
  * @brief Adds a link between two tensors in the computational graph.
@@ -70,7 +70,7 @@ cgrad_error add_computational_graph_link(tensor* operand, size_t operand_id, ten
  * @param child The child node to add.
  * @return NO_ERROR if successful, otherwise an appropriate error code.
  */
-cgrad_error add_child(computational_graph_node *const node, computational_graph_node *const child);
+cgrad_error add_child(struct computational_graph_node *const node, struct computational_graph_node *const child);
 
 /**
  * @brief Adds a parent node to a computational graph node.
@@ -80,13 +80,13 @@ cgrad_error add_child(computational_graph_node *const node, computational_graph_
  * @param operand The operand associated with the parent.
  * @return NO_ERROR if successful, otherwise an appropriate error code.
  */
-cgrad_error add_parent(computational_graph_node *const node, computational_graph_node *const parent, const size_t operand);
+cgrad_error add_parent(struct computational_graph_node *const node, struct computational_graph_node *const parent, const size_t operand);
 
 /**
  * @brief Prints the details of a computational graph node.
  *
  * @param node The node to print.
  */
-void print_computational_graph_node(const computational_graph_node *node);
+void print_computational_graph_node(const struct computational_graph_node *node);
 
 #endif
--- src/autograd/backpropagation.c
+++ src/autograd/backpropagation.c
@@ -7,14 +7,14 @@
 
 typedef struct 
 {
-    computational_graph_node* targets[AUTOGRAD_MAX_TARGETS];
+    struct computational_graph_node* targets[AUTOGRAD_MAX_TARGETS];
     size_t size;
 } backpropagation_targets;
 
-static void identify_backpropagation_nodes(computational_graph_node* const node, backpropagation_targets* targets);
-static tensor* build_gradient(computational_graph_node* const node);
+static void identify_backpropagation_nodes(struct computational_graph_node* const node, backpropagation_targets* targets);
+static tensor* build_gradient(struct computational_graph_node* const node);
 static void build_gradients(backpropagation_targets* const targets);
-cgrad_error add_target(backpropagation_targets* const targets, computational_graph_node* const node);
+cgrad_error add_target(backpropagation_targets* const targets, struct computational_graph_node* const node);
 void set_gradient_wrt_itself(tensor* const t);
 
 void backward(tensor* t, bool retain_graph)
@@ -32,13 +32,13 @@ void backward(tensor* t, bool retain_graph)
 
     for (size_t i = 0; i < targets.size; i++)
     {
-        computational_graph_node* node = targets.targets[i];
+        struct computational_graph_node* node = targets.targets[i];
         node->t->node = NULL;
         free_computational_graph_node(node);
     }
 }
 
-static void identify_backpropagation_nodes(computational_graph_node* const node, backpropagation_targets* targets)
+static void identify_backpropagation_nodes(struct computational_graph_node* const node, backpropagation_targets* targets)
 {
     node->is_involved_in_backprop = true;
     add_target(targets, node);
@@ -46,7 +46,7 @@ static void identify_backpropagation_nodes(computational_graph_node* const node,
         identify_backpropagation_nodes(node->children[i], targets);
 }
 
-static tensor* build_gradient(computational_graph_node* const node)
+static tensor* build_gradient(struct computational_graph_node* const node)
 {
     if (node->is_grad_computed)
     {
@@ -62,7 +62,7 @@ static tensor* build_gradient(computational_graph_node* const node)
 
         tensor* D = build_gradient(node->parents[i]);
 
-        computational_graph_node *parent_node = node->parents[i];
+        struct computational_graph_node *parent_node = node->parents[i];
         const tensor **const operands = (const tensor** const)parent_node->tensor_operands;
 
         // Get which is the operand of the current node in the operation
@@ -94,7 +94,7 @@ static void build_gradients(backpropagation_targets* const targets)
     }
 }
 
-cgrad_error add_target(backpropagation_targets* const targets, computational_graph_node* const node)
+cgrad_error add_target(backpropagation_targets* const targets, struct computational_graph_node* const node)
 {
     size_t const size = targets->size;
     if (size >= AUTOGRAD_MAX_TARGETS)
