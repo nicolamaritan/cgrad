cfb44df8bcfdc47d57342e3232837bd88de8b970
Nicola Maritan <nicolamaritan01@gmail.com>
2025-07-19 22:06:35 +0200

    Vectorized relu

--- include/layers/relu.h
+++ include/layers/relu.h
@@ -7,5 +7,6 @@
 
 cgrad_error relu_forward_graph(struct tensor *const x, struct tensor *const out, struct autograd_allocators *ag_allocators);
 cgrad_error relu_forward(const struct tensor *const x, struct tensor *const out);
+void relu_forward_unchecked(const struct tensor *const x, struct tensor *const out);
 
 #endif
